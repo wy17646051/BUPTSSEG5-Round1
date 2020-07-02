20b99b9a68ae6d87fbf3690fffffd68dae46adfd
StrugglePuppet <66124661+StrugglePuppet@users.noreply.github.com>
2020-07-02 17:37:51 +0800

    Update main.cpp

--- 杨皓晨/A/main.cpp
+++ 杨皓晨/A/main.cpp
@@ -2,8 +2,8 @@
 #include<algorithm>
 using namespace std;
 
-int a[101];
-int p[101];
+int a[101]; //存放所有的待交换的数
+int p[101]; // 位置数组
 
 int main()
 {
@@ -18,11 +18,11 @@ int main()
         for(int j = 1;j <= m;j++){
             scanf("%d",&p[j]);
         }
-        sort(p + 1,p + m + 1);
-        sort(a + p[1],a + p[m]);
+        sort(p + 1,p + m + 1);  //将位置数组排序，得到最大与最小位置
+        sort(a + p[1],a + p[m]);  //将最大与最小之间的数排序
         bool flag = false;
         if(p[1] > 1){
-            for(int i = 1;i < p[1];i++){
+            for(int i = 1;i < p[1];i++){ //若最小交换位置大于1，且其小于最小交换位置的不能交换到所有数存在降序，则输出NO；
                 if(a[i] > a[i + 1]){
                     flag = true;
                     break;
@@ -30,7 +30,7 @@ int main()
             }
         }
         if(p[m] < n - 1){
-            for(int i = n;i > p[m];i--){
+            for(int i = n;i > p[m];i--){  //若最大交换位置小于倒数第二个，且其大于最大交换位置的不能交换到所有数存在降序，则输出NO； 
                 if(a[i] < a[i - 1]){
                     flag = true;
                     break;
