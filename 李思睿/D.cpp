02f761b00d14ccb736a136c9310f937cbb970120
Qinphy <Qinphy@126.com>
2020-07-16 17:34:52 +0800

    补做：D和E,并添加注释

--- 李思睿/D.cpp
+++ 李思睿/D.cpp
@@ -5,29 +5,36 @@
 using namespace std;
 
 char str[MAX_N];
+int v[3] = {0};
 
 // past test1-8
+// 错误点：没有考虑中间的数字能够%3==0的情况
+/** 基本思路：借助数字每位数之和能够整除3，则该数字即可整除3
+    用数组记录3的余数0，1，2：如果余数两次被访问，就说明能被整除了
+    两次访问意味着：在之前记录的数字基础上，加了3的倍数，说明中间的数字可以整除
+*/
+// 当前代码能够accept：在codeforces提交成功，用户xiaoboyu，编号86973549
 int main(void)
 {
+    // 用字符串存储大数字
 	cin>>str;
-	
+
 	int n = strlen(str);
-	int sum = 0;
-	int cnt = 0;
-	int cnt_x = 0;
-	for (int i = 0; i < n; i++) {
-		sum += (str[i] - '0');
-		if (str[i] - '0' == 0) cnt_x++;
-		if (sum % 3 == 0) {
-			sum = 0;
-			
-			if (cnt_x > 1) cnt += cnt_x;
-			else cnt++;
-			
-			cnt_x = 0;
-		}
-	}
-	cnt += cnt_x; 
+    int cnt = 0;
+    int x = 0;
+    p[0] = 1;   // 防止第一个数字被忽略
+    for (int i = 0; i < n; i++) {
+        x += (str[i] - '0');
+        x %= 3;
+        if (v[x]) {
+            v[0] = 1;
+            v[1] = 0;
+            v[2] = 0;
+            x = 0;
+            cnt++;
+        } else v[x] = 1;
+    }
+
 	cout<<cnt<<endl;
 	return 0;
 }
--- 李思睿/E.cpp
+++ 李思睿/E.cpp
@@ -4,37 +4,56 @@
 #define MAX_N 200000 + 5
 using namespace std;
 
-int a[MAX_N] = {0};
-int b[MAX_N] = {0};
-int v[MAX_N] = {0};
+int a[MAX_N] = {0}; // 记录输入
+int b[MAX_N] = {0}; // 记录数字的个数
+int v[MAX_N] = {0}; // 访问过的数字
 
-
-// pass: test1 test2
+// 提交的时候只pass test1-2
+// 错误点：数字忽略了整体左右移动的情况
+// 现在的代码能够 accept：在codeforces提交成功，用户xiaoboyu，编号86970922
 int main(void)
 {
 	int n;
 	cin>>n;
 	memset(b, 0, sizeof(b));
 	memset(v, 0, sizeof(v));
+
+	// 输入，并记录每个数字的个数
 	for (int i = 0; i < n; i++) {
 		cin>>a[i];
 		b[a[i]]++;
 	}
-	
+
+	// 通过排序，排除相同的数字，防止重复计算
 	sort(a, a + n);
 	int m = unique(a, a + n) - a;
-	
+
 	v[0] = 1;
 	for (int i = 0; i < m; i++) {
-		v[a[i]] = 1;
-		if (b[a[i]] >= 3) {
-			v[a[i] - 1] = 1;
-			v[a[i] + 1] = 1;
-		} else if (b[a[i]] == 2) {
-			if (v[a[i] - 1] == 0) v[a[i] - 1] = 1;
-			else if (v[a[i] + 1] == 0) v[a[i] + 1] = 1;
-		}
-	}
+    		if (b[a[i]] >= 3) {
+    		// 个数大于3个的数字，+1、-1和自己都可以添加
+    			v[a[i] - 1] = 1;
+    			v[a[i] + 1] = 1;
+    			v[a[i]] = 1;
+    		} else if (b[a[i]] == 2) {
+    		// 个数等于2个的数字，尽量填充左边没访问过的数字
+    			if (v[a[i] - 1] == 0) v[a[i] - 1] = 1;
+    			else if (v[a[i] + 1] == 0) v[a[i] + 1] = 1;
+    			v[a[i]] = 1;
+    		} else if (b[a[i]] == 1) {
+    		// 个数等于1个的数字，尽量填满左边的数字-1
+    			if (v[a[i] - 1] == 0) {
+    				v[a[i]] = 0;
+    				v[a[i] - 1] = 1;
+    			} else if (v[a[i]] != 0) {
+    			// 如果自己被左边的数字访问过了，就往右边填充。
+    				v[a[i] + 1] = 1;
+    			} else {
+    			// 没法左右移动就填自己的位置
+    				v[a[i]] = 1;
+    			}
+    		}
+    	}
 	
 	int cnt = 0;
 	for (int i = 1; i <= 150001; i++) {
