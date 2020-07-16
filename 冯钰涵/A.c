58d246b942459f2da773d9c3dd72ccb1f2238272
111563421 <61934371+111563421@users.noreply.github.com>
2020-07-16 17:23:11 +0800

    Update A.c

--- 冯钰涵/A.c
+++ 冯钰涵/A.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 //test 5 run out of time limit
+//主要思路为先判断m能不能被n整除，如果可以才可能有结论，如果不行返回-1，如果n=m则返回0
+//如果能被整除，则让m除n，再让计算后的商被3和2相除，根据上面的判断，这里m除n一定能整除
+//利用while循环把商一次一次整除，直到不能被整除位置，每次循环计数+1，最终计数为结论
+
 int main() {
     int n,m,result=1,temp;
     scanf("%d%d",&n,&m);
     int i=0;
-    if(n==m){
+    if(n==m){//判断是否相等，相等返回0
         printf("0");
     }
-    else if(m%n==0){
+    else if(m%n==0){//判断能否整除
         temp=m/n;
         while(i==0) {
             if (temp % 3 == 0) {
@@ -25,10 +29,11 @@ int main() {
                 }
             }
         }
-        printf("%d",result);
+        printf("%d",result);//返回计数即结论
     }
-    else{
+    else{//不能整除，返回-1
         printf("-1");
     }
 
 }
+
